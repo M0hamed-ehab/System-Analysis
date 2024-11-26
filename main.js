b1ca20cb529fa04785252cb49947b19b4213cbb1
M0hamed-ehab <mohamedehab131204@gmail.com>
2024-11-26 22:17:33 +0200

    BORROW PAGE

--- main.js
+++ main.js
@@ -120,4 +120,90 @@ function removePopup() {
 }
 
 
+
+
+function AccForm() {
+    const X = document.getElementById('savedAccountForm');
+    const y = document.getElementById('newAccountForm');
+    const Z = document.getElementById('newAccount').checked;
+
+    if (Z) {
+        X.style.display = "none";
+        y.style.display = "block";
+    } else {
+        y.style.display = "none";
+        X.style.display = "block";
+    }
+}
+
+function selectPayment(X) {
+    const S = document.getElementById('SBB');
+    const N = document.getElementById('NBB');
+    const C = document.getElementById('CBB');
+    const NC = document.getElementById('NCBB');
+
+
+    [S, N, C].forEach(el => el.classList.remove('selected'));
+
+
+    NC.style.display = "none";
+
+    switch (X) {
+        case 'S':
+            S.classList.add('selected');
+            break;
+        case 'N':
+            N.classList.add('selected');
+            NC.style.display = "block";
+            break;
+        case 'C':
+            C.classList.add('selected');
+            break;
+    }
+}
+
+function updateTotal() {
+    const X = document.querySelector('input[name="deliveryMethod"]:checked').value;
+    const F = X === 'home' ? 2.00 : (X == "locker" ? 5.00 : 0.00);
+    document.getElementById('deliveryFee').textContent = `Delivery Fee: $${F.toFixed(2)}`;
+    document.getElementById('totalAmount').textContent = `$${(20 + F).toFixed(2)}`;
+}
+
+
+function confirmBorrowing(event) {
+    event.preventDefault();
+    const X = document.getElementById('newAccount').checked;
+    const Y = X ? document.getElementById('newMemberID').value : document.getElementById('memberID').value;
+    const E = X ? document.getElementById('newEmail').value : document.getElementById('email').value;
+    const deliveryMethod = document.querySelector('input[name="deliveryMethod"]:checked').value;
+    const N = {
+        email: document.getElementById('emailN').checked,
+        sms: document.getElementById('smsN').checked,
+        reminders: document.getElementById('remN').checked
+    };
+
+    if (X && (!Y || !E)) {
+        popup();
+        return;
+    }
+
+    const P = document.querySelector('.LBDPSMC.selected').id;
+
+    if (P === 'NBB') {
+        const CN = document.getElementById('cardNumber').value;
+        const ED = document.getElementById('expiry').value;
+
+        if (!CN || !ED) {
+            popup();
+            return;
+        }
+    }
+    document.getElementById("POPUP").textContent = "BORROWING IS COMPLETED";
+    popup();
+
+
+}
+
+
+
 //-------------------------------------------------------Mohamed part ending---------------------------------------------------------
